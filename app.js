3abfee09d62e2dc5cc6ea6889b120827781bd3f1
Sanna BS Jammeh <sannabsjammeh54@gmail.com>
2024-08-30 22:35:19 -0700

    SETTING UP EXPRESS MODULES

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate')
+const path = require('path');
+const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/Neigbor')
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
